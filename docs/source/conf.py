6a47ad8c33bdfa992344f765fa06fcc2b10af0b9
WenyiWU <98112196+WenyiWU0111@users.noreply.github.com>
2023-10-14 19:15:10 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ author = 'WenyiWU'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+source_dir = 'source'
 master_doc = 'index'
 extensions = ['sphinx.ext.duration','sphinx.ext.mathjax']
 
